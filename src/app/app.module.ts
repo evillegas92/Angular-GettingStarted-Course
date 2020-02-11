470aee7f669e39ee8fed6b5f5fabd6d5554261ef
Esteban Villegas <evillegas@optimalblue.com>
2020-02-11 08:55:30 -0500

    Added the feature module Product module and the shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { StarComponent } from './shared/star.component';
-import { ProductDetailComponent } from './products/product-detail.component';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule,
     RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', canActivate: [ProductDetailGuard], component: ProductDetailComponent },
       { path: 'welcome', component: WelcomeComponent },
       { path: '', redirectTo: 'welcome', pathMatch: 'full' }, //default route that redirects to our welcome component.
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' } //wild-card in case the url doesn't match any configured route.
-    ])
+    ]),
+    ProductModule
   ],
   bootstrap: [AppComponent]
 })
